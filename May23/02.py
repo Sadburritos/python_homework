7218afbeb7a469d10bc40ea31b271deba6d22576
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-06 17:42:08 +0600

    Проверено дз от 23.05
    Комментарии к коду

--- May23/02.py
+++ May23/02.py
@@ -7,6 +7,7 @@ third_char = (text[2])
 
 
 massage = first_char + second_char + third_char
+# VN: нужна сумма кодов символов. Т.е. ord(first_char) + ord(second_char) + ord(third_char)
 
 print(ord(text[0]), ord(text[1]), ord(text[2]))
 print(massage)
--- May23/07.py
+++ May23/07.py
@@ -7,3 +7,13 @@ message_template = "%s.%s.%s"
 message = message_template % (year, month, day)
 
 print (message)
+
+'''
+год 2010
+месяц 3
+число рождения 3
+2010.3.3
+
+Должно быть: 03.03.2010
+т.е. формат  дд.мм.гггг
+'''
